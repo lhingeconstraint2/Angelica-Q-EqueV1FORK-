4c894002a721a37079699ea401e1729896f8b476
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2024-04-22 11:34:50 +0200

    init

---
+++ DiscordEqueBot/Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    public static async Task Main()
+    {
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.All
+        };
+        _client = new DiscordSocketClient(config);
+
+        _client.Log += Log;
+        _client.Ready += Ready;
+        _client.MessageUpdated += MessageUpdated;
+
+
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private static Task Ready()
+    {
+        Console.WriteLine($"Connected as -> [{_client.CurrentUser.Username}]");
+        return Task.CompletedTask;
+    }
+
+    private static async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after,
+        ISocketMessageChannel channel)
+    {
+        // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+        var message = await before.GetOrDownloadAsync();
+        Console.WriteLine($"{message} -> {after.Content}");
+    }
+}
